e770a0c2cc21685312c3dff7617af9addffb25ad
Shantanu Radke <saketradke123@gmail.com>
2020-04-04 20:21:11 +0530

    controlFlow/if-else

---
+++ CS(basic)/if_else/if_else/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else
+{
+   
+
+    class Program
+    {
+        public enum season
+        {
+            summer,
+            rainy,
+            winter,
+            spring
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            var ThisSeason = season.summer;
+
+            switch (ThisSeason)
+            {
+                case season.summer:
+                    Console.WriteLine("This is hot");
+                    break;
+                case season.winter:
+                    Console.WriteLine("This is cold");
+                    break;
+                case season.spring:
+                    Console.WriteLine("This is beautifull");
+                    break;
+                case season.rainy:
+                    Console.WriteLine("This is rainy");
+                    break;
+                default:
+                    Console.WriteLine("Unknown");
+                    break;
+
+
+
+
+            }
+
+        }
+    }
+}
