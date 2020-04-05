978559f833beada263cd33a85ff9e5d5a946e828
Shantanu Radke <saketradke123@gmail.com>
2020-04-05 12:53:41 +0530

    Iteration/ForEach

---
+++ CS(basic)/Iteration_ForEach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration_ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "chris hemesworth";
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+        }
+    }
+}
