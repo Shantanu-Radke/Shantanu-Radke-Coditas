cc0a7491cbcff038a31a39baae2e6d837152384f
Shantanu Radke <saketradke123@gmail.com>
2020-04-05 18:52:45 +0530

    #creating password using Random class

---
+++ CS(basic)/Iteration_RandomClass/Program.cs
@@ -0,0 +1,24 @@
+﻿//Using Random class to generate a password with help of ascii numbers of characters
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration_RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = new Random();
+            for (var i = 0 ; i < 10 ; i++)
+            {
+                Console.Write((char)str.Next(97, 120));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
