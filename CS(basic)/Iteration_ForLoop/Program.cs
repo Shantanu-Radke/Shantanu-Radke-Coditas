253522abfde234944a3163035df6077aa0af94a7
Shantanu Radke <saketradke123@gmail.com>
2020-04-05 12:37:26 +0530

    #Iteration/forloop/star_triangle

---
+++ CS(basic)/Iteration_ForLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration_ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 10; i++)
+            {
+                for (var j = 1; j <= i; j++)
+                {
+                    Console.WriteLine("*");
+                }
+
+                Console.WriteLine("\n");
+            }
+
+        }
+    }
+}
