74d6870704dee4cb24951e7c2842f5633ab0fb08
Shantanu Radke <saketradke123@gmail.com>
2020-04-05 19:39:15 +0530

    Iteration/Exercise3

---
+++ CS(basic)/Iteration_Exercise3/Program.cs
@@ -0,0 +1,27 @@
+﻿//Find the factorial of enterd number
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration_Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j=1;
+            Console.WriteLine("Enter the num");
+            i = Convert.ToInt16(Console.ReadLine());
+
+            while (i != 0)
+            {
+                j = i * j;
+                i--;
+            }
+
+            Console.WriteLine("Factorial is " + j);
+        }
+    }
+}
