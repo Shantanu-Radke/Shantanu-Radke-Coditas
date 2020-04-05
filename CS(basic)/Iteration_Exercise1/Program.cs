d88ccfdb01be16d13657ecd673075bf552801e49
Shantanu Radke <saketradke123@gmail.com>
2020-04-05 19:03:56 +0530

    #count of no. divisible by 3 between (1-100)

---
+++ CS(basic)/Iteration_Exercise1/Program.cs
@@ -0,0 +1,27 @@
+﻿//count how many numbers divisible by 3 between 1 to 100
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j=0;
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    j++;
+                }
+                
+   
+            }
+            Console.WriteLine(j + " Numbers are divisible by 3 ");
+        }
+    }
+}
