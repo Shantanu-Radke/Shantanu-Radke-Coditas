147e081684d7e95530757fe88c69d5b022234020
Shantanu Radke <saketradke123@gmail.com>
2020-04-05 10:16:49 +0530

    switch_case

---
+++ CS(basic)/SwitchCase/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        public enum season
+        {
+            summer,
+            winter,
+            rainy,
+            spring
+        }
+
+
+        static void Main(string[] args)
+        {    
+            var ThisSeason=season.summer;
+
+            switch (ThisSeason)
+            {
+                case season.summer:
+                    Console.WriteLine("It is hot ");
+                    break;
+
+
+                    case season.winter:
+                    Console.WriteLine("It is cold ");
+                    break;
+
+                    case season.rainy:
+                    Console.WriteLine("It is rainy");
+                    break;
+
+                    case season.spring:
+                    Console.WriteLine("It is beautiful ");
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown");
+                    break;
+
+
+
+
+
+
+            }
+
+        }
+    }
+}
