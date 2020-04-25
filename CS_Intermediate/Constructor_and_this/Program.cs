085f6fdb1abe8de2c7a0a550930d596185b2e9d1
Shantanu Radke <saketradke123@gmail.com>
2020-04-25 11:32:31 +0530

    Constructor Demo

---
+++ CS_Intermediate/Constructor_and_this/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_and_this
+{
+    public class point
+    {
+        int x;
+        int y;
+
+        public point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void m1()
+        {
+            Console.WriteLine("Coordinate x = " + x +" Coordinate y " + y );
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            point p = new point(20, 30);
+            p.m1();
+        }
+    }
+}
