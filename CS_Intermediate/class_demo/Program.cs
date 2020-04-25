2f87b345ee9687ca8fee8ab21c8b9b241cec121c
Shantanu Radke <saketradke123@gmail.com>
2020-04-25 11:31:17 +0530

    #class demo

---
+++ CS_Intermediate/class_demo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_demo
+{
+    public class Person
+    {
+
+        public String name;
+        
+
+        public void introduce(string name1)
+        {
+            Console.WriteLine("Hello " + name + " I am " + name1);
+        }
+
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.name = "Stark";
+            p.introduce("Mosh");
+        }
+    }
+}
